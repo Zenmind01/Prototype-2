c146d627539b74c2653e6df3b0abdf8b7fde314f
Gaurav Yadav <65115466+gauravyad86@users.noreply.github.com>
2024-09-20 19:26:10 +0530

    Replaced openAI with Gemini

--- src/components/User-Dash/Chats/config.js
+++ src/components/User-Dash/Chats/config.js
@@ -1,5 +1,5 @@
 const config = {
-  apiKey: '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'
+  apiKey: 'AIzaSyBqTwXHEDRBVQZ9hcVPc1AUzeejzDbeaX8'
 };
 
 export default config;
